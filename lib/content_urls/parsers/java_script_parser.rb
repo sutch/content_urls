7a5bdd0e6f1da5b50e860c1ed76dba83522a61e2
Dennis Sutch <dennis@sutch.com>
2012-09-30 15:03:42 -0400

    Documented JavaScript parser.

--- lib/content_urls/parsers/java_script_parser.rb
+++ lib/content_urls/parsers/java_script_parser.rb
@@ -1,15 +1,39 @@
 require 'uri'
 
 class ContentUrls
+  # +JavaScriptParser+ finds and rewrites URLs in JavaScript content.
+  #
+  # === Implementation note:
+  # This methods in this class identify URLs by locating strings which match +URI+'s regexp.
   class JavaScriptParser
 
-    def self.each_url(content)
+    # Returns the URLs found in the JavaScript content.
+    #
+    # @param [String] content the JavaScript content.
+    # @return [Array] the unique URLs found in the content.
+    #
+    # @example Parse JavaScript code for URLs
+    #   javascript = 'var link="http://example.com/"'
+    #   ContentUrls::JavaScriptParser.urls(javascript).each do |url|
+    #     puts "Found URL: #{url}"
+    #   end
+    #   # => "Found URL: http://example.com/"
+    def self.urls(content)
       urls = []
       URI.extract(content).each { |u| urls << u }
       urls.uniq!
       urls
     end
 
+    # Rewrites each URL in the JavaScript content by calling the supplied block with each URL.
+    #
+    # @param [String] content the JavaScript content.
+    #
+    # @example Rewrite URLs in JavaScript code
+    #   javascript = 'var link="http://example.com/"'
+    #   javascript = ContentUrls::JavaScriptParser.rewrite_urls(javascript).each {|url| url.upcase}
+    #   # => "Rewritten: var link="HTTP://EXAMPLE.COM/""
+    #
     def self.rewrite_each(content, &block)
       done = false
       remaining = content
